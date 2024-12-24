99646ac1bcfd123b07ebddc4e254e88bc64337d6
Pornchanit2203 <pornchanit.money@gmail.com>
2024-12-24 20:23:22 +0700

    Delete Lectuer03/JS-Intro03-main directory

--- Lectuer03/JS-Intro03-main/test1.js
+++
@@ -1,7 +0,0 @@
-let values1 = ['Apple', 1, false];
-let values2 = ['Fries', 2, true];
-let values3 = ['Mars', 9, 'Apple'];
-
-let commonElements = values1.filter(item => values2.includes(item) || values3.includes(item));
-
-console.log('Common Elements:', commonElements);
